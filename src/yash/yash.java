16e1019d70799aeec0775addce9f094ff66c744a
yashp <yashp@10.16.11.50>
2023-06-20 14:43:19 -0400

    new java file

---
+++ src/yash/yash.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yash;
+
+/**
+ *
+ * @author yashp
+ */
+public class yash {
+    / This is a sample class created by yash.
+    // It demonstrates the usage of comments in Java.
+
+    public static void main(String[] args) {
+        // This is the entry point of the program.
+        // It will be executed when the program is run.
+        System.out.println("Hello, World!");
+    }
+}
+
